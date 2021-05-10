ba1bd47d57cfdce580a2fb9cb13fd3225908cb89
Freddy Yang <freddyyang@SYNOLOGY.INC>
2021-05-10 17:37:30 +0800

    first comit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Usage
+
+1. Put all images in `input` dictionary.
+2. execute command:
+
+```shell=
+node index.js
+```
---
+++ index.js
@@ -0,0 +1,36 @@
+const Jimp = require('jimp');
+const path = require('path');
+const fs = require('fs');
+
+function rename(file) {
+  const irregularSymol = /[ -!$%^&*()_+|~=`{}\[\]:";'<>?,\/]/g;
+  return file.replace(irregularSymol, '_').toLowerCase();
+}
+
+async function execute() {
+  const root = './input/';
+  const directoryPath = path.join(__dirname, root);
+
+  //passsing directoryPath and callback function
+  fs.readdir(directoryPath, (err, files) => {
+    //handling error
+    if (err) {
+      return console.log('Unable to scan directory: ' + err);
+    }
+    //listing all files using forEach
+    files.forEach(async (file) => {
+      // load local image file with jimp. It supports jpg, png, bmp, tiff and gif:
+      await Jimp.read(`${root}${file}`, (err, inputImg) => {
+        if (err) throw err;
+
+        inputImg
+          .quality(60)
+          .write(`output/${rename(file)}`);
+      });
+    });
+
+    console.log('done.');
+  });
+}
+
+execute();
