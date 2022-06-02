288d44bb344b526f98e3e75d51704049f45a488b
FreddyCro <freddy10100@hotmail.com>
2022-06-02 16:02:02 +0800

    feat(webp): generate info file

--- src/run-webp.js
+++ src/run-webp.js
@@ -14,6 +14,8 @@ import { runRename } from './run-rename.js';
 
 const { Input } = require('enquirer');
 
+let imagesInfo = [];
+
 const renameOutputWebp = (file) => {
   if (!file) return;
 
@@ -21,7 +23,10 @@ const renameOutputWebp = (file) => {
   const newName = rename2x(name);
 
   try {
-    fs.renameSync(normalizePath(file), normalizePath(`${dir}/${newName}${ext}`));
+    fs.renameSync(
+      normalizePath(file),
+      normalizePath(`${dir}/${newName}${ext}`)
+    );
 
     console.log(
       `${name + ext} has converted to ${chalk.green(name + '.webp')}`
@@ -48,7 +53,7 @@ const handleWebp = (quality) => {
           destination: normalizePath(`${OUTPUT_DIR}/${newDir}`),
           plugins: [imageminWebp({ quality })],
           method: 6,
-        }).then((e) => {
+        }).then(async (e) => {
           if (e.length === 0) {
             consola.error(`${chalk.red(name + ext)} was failure.`);
           } else {
@@ -62,6 +67,41 @@ const handleWebp = (quality) => {
 
     Promise.all(pAll).then(() => {
       consola.success('All files was done.');
+    });
+  } catch (e) {
+    consola.error(e);
+  }
+
+  // handle generate image info
+  try {
+    const pAll = files.map((file) => {
+      if (file.includes('@2x')) return;
+
+      return new Promise((resolve, reject) => {
+        Jimp.read(file).then((img) => {
+          const { dir, name, ext } = pathParse(file);
+
+          const info = {
+            src: `${name}`,
+            width: img.bitmap.width,
+            height: img.bitmap.height,
+          };
+
+          imagesInfo.push(info);
+          resolve();
+        });
+      });
+    });
+
+    Promise.all(pAll).then(() => {
+      fs.writeFile(
+        normalizePath(`${OUTPUT_DIR}/info.json`),
+        JSON.stringify(imagesInfo, null, 2),
+        (err) => {
+          if (err) console.log(err);
+          consola.success('All info was writed.');
+        }
+      );
     });
   } catch (e) {
     consola.error(e);
